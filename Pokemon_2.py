ca020fb5e05f2332938ea3ac1ee8832091cdec9b
sgomezp <sgomezp@gmail.com>
2023-07-02 15:50:11 +0200

    Resuelvo que no puedan pelear dos pokemons de la misma casa

--- Pokemon_2.py
+++ Pokemon_2.py
@@ -2,8 +2,9 @@
 from tabulate import tabulate
 import random
 
-#Crear dictionary para guardar los life_points de todos los pokemos: sirve para poder resetear
+# Crear dictionary para guardar los life_points de todos los pokemos: sirve para poder resetear
 life_points_dic = {}
+
 
 # Clase principal (parent)
 class Pokemon:
@@ -59,19 +60,24 @@ class Pokemon:
         flag = True
         while flag:
             if self.attacks:
-                random_attack = random.choice(list(self.attacks.keys()))
-                print(f"{self.name} usa {random_attack} contra {target.name}")
-                power_attack = self.attacks[random_attack]
-                result = self.cal_points(target, power_attack)
-                lp_temp = target.life_points
-                if result <= 0:
-                    print(f"{self.name} le ganó a {target.name}")
-                    flag = False
-                    target.life_points = life_points_dic[target.name]
+                if self.pk_type == target.pk_type:
+                    print("Son del mismo tipo. No pueden pelear")
+                    break
                 else:
-                    print(f"{target.name} ha sobrevivido al ataque de {self.name} quedando sus Life Points en: {result} \n")
+                    random_attack = random.choice(list(self.attacks.keys()))
+                    print(f"{self.name} usa {random_attack} contra {target.name}")
+                    power_attack = self.attacks[random_attack]
+                    result = self.cal_points(target, power_attack)
                     lp_temp = target.life_points
-                    target.life_points = result
+                    if result <= 0:
+                        print(f"{self.name} le ganó a {target.name}")
+                        flag = False
+                        target.life_points = life_points_dic[target.name]
+                    else:
+                        print(
+                            f"{target.name} ha sobrevivido al ataque de {self.name} quedando sus Life Points en: {result} \n")
+                        lp_temp = target.life_points
+                        target.life_points = result
 
 
 # Child classes
@@ -82,6 +88,7 @@ class Agua(Pokemon):
         self.weakeness = ["Planta"]
         self.strength = ["Fuego"]
 
+
 class Fuego(Pokemon):
     def __init__(self, name, life_points):
         super().__init__(name, "Fuego", life_points)
@@ -91,11 +98,12 @@ class Fuego(Pokemon):
 
 
 class Planta(Pokemon):
-    def __init__(self,name, life_points):
+    def __init__(self, name, life_points):
         super().__init__(name, "Planta", life_points)
         super().add_attack("Látigo cepa", 45)
         self.weakeness = ["Fuego"]
         self.strength = ["Agua"]
+
 
 def arma_combate():
     """
@@ -112,10 +120,11 @@ def arma_combate():
         if nombre_retador in globals() and nombre_objetivo in globals():
             retador = globals()[nombre_retador]
             objetivo = globals()[nombre_objetivo]
-            # Llamar al método attack() del Pokémon retador pasando el Pokémon objetivo como argumento
             retador.attack(objetivo)
             print()
             seguir = input("Desea otro combate: s/n: ").lower()
+            while seguir not in ["s", "n"]:
+                seguir = input("Caractéres no válidos. Debe introducir s/n para continuar: ").lower()
         else:
             print("Los nombres de los Pokémon no son válidos.")
         if seguir != "s":
@@ -139,10 +148,11 @@ def crea_tabla():
     encabezado = ["Agua", "Fuego", "Planta"]
 
     # Genero tabla
-    tabla = tabulate(datos, headers = encabezado, tablefmt="fancy_grid")
+    tabla = tabulate(datos, headers=encabezado, tablefmt="fancy_grid")
 
     # Imprimir tabla
     print(tabla)
+
 
 # Crear instancias de la clase Pokemon Agua
 squirtle = Agua("Squirtle", 44)
@@ -157,14 +167,14 @@ treecko = Planta("Treecko", 40)
 # Crear instancias de la clase Fuego
 torchic = Fuego("Torchic", 45)
 cyndaquil = Fuego("Cyndaquil", 39)
-charmander = Fuego("Chamander",39)
+charmander = Fuego("Chamander", 39)
 
 # Añado ataque Burbuja a Squirtle y a Totodile
 squirtle.add_attack("Burbuja", 40)
 totodile.add_attack("Burbuja", 40)
 
 # Añado ataque Llama Azul a Cyndaquil y Lluvia ígena a Charmander
-cyndaquil.add_attack("Llama azul",130)
+cyndaquil.add_attack("Llama azul", 130)
 charmander.add_attack("Lluvia ígnea", 100)
 
 # Añado ataque Bomba germen a Treecko y Hoja aguda a Chikorita
@@ -172,9 +182,4 @@ treecko.add_attack("Bomba germen", 55)
 chikorita.add_attack("Hoja aguda", 70)
 
 crea_tabla()
-
 arma_combate()
-
-
-
-
